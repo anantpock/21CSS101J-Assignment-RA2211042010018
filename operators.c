db267c63bf7df8a916018a7ec19b50d67cb13721
anantpock <113873476+anantpock@users.noreply.github.com>
2022-10-05 00:56:24 +0530

    Create operators.c

---
+++ operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a = 19,b = 7, c;
+    
+    c = a+b;
+    printf("a+b = %d \n",c);
+    c = a-b;
+    printf("a-b = %d \n",c);
+    c = a*b;
+    printf("a*b = %d \n",c);
+    c = a/b;
+    printf("a/b = %d \n",c);
+    c = a%b;
+    printf("Remainder when a divided by b = %d \n",c);
+    
+    return 0;
+}
