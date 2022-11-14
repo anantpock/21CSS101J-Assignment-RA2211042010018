6ecb5020f33224d846728d0f237539bd2810d6a7
anantpock <113873476+anantpock@users.noreply.github.com>
2022-11-14 10:17:20 +0530

    Create DOUBLE.c

---
+++ DOUBLE.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() 
+  
+{
+  printf("RA2211042010018");
+return 0;
+}
+{
+   int i;
+   double number, sum = 0.0;
+
+   for (i = 1; i <= 10; ++i) {
+      printf("Enter n%d: ", i);
+      scanf("%lf", &number);
+
+      // if the user enters a negative number, break the loop
+      if (number < 0.0) {
+         break;
+      }
+
+      sum += number; // sum = sum + number;
+   }
+
+   printf("Sum = %.2lf", sum);
+
+   return 0;
+}
