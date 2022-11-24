5a22d3cd7027cc287d61acad1b497b4f0da86660
anantpock <113873476+anantpock@users.noreply.github.com>
2022-11-24 14:38:36 +0530

    Create odd or even.c

---
+++ odd or even.c
@@ -0,0 +1,22 @@
+/*odd evennumber*/
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    printf("enter number");
+    int a;
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("even number");
+    }
+    else
+    {
+        printf("odd number");
+    }
+}
+/*output:
+enter number67
+odd number
+ */
