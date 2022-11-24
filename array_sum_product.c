df90fcd004f8d42dd82247a041c36b2e6372a65e
anantpock <113873476+anantpock@users.noreply.github.com>
2022-11-24 14:32:04 +0530

    Add files via upload

---
+++ array_sum_product.c
@@ -0,0 +1,27 @@
+/*find sum and product of two array*/
+#include <stdio.h>
+
+main()
+{
+  printf("RA2211042010027\n");
+  int A[5],B[5],C[5],D[5];
+  printf("enter 5 elements of A\n");
+  for(int a=0;a<5;a++)
+  {
+      scanf(" %d", &A[a]);
+  }
+   printf("enter 5 elements of B\n");
+  for(int a=0;a<5;a++)
+  {
+      scanf(" %d", &B[a]);
+  }
+  for (int a=0;a<5;a++)
+  {
+      C[a]=A[a]+B[a];
+      D[a]=A[a]*B[a];
+  }
+  for (int a=0;a<5;a++)
+  {
+      printf(" %d %d\n",C[a],D[a]);
+  }
+}
